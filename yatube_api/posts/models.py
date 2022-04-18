bcf1ab51a9c0a418ed66f41c2e16e0b1c1120635
sproggi <78502878+sproggi@users.noreply.github.com>
2022-04-18 21:40:12 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-User = get_user_model()
+
+User = get_user_model() # стандартная модель User в Django
 
 
 class Group(models.Model):
