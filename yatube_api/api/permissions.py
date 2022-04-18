0787e3658a6696d702ea3f929f4872ae6b44b24b
sproggi <78502878+sproggi@users.noreply.github.com>
2022-04-18 21:41:29 +0300

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
+    """Проверка прав на чтение или авторство постов."""
     message = "Изменение или удаление чужого контента запрещено!"
 
     def has_object_permission(self, request, view, obj):
